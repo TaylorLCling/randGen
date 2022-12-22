6b3be244f3c686a49fc7add24e66f29031caca63
Taylor Clingenpeel <TaylorClingenpeel@gmail.com>
2022-12-21 19:02:12 -0500

    Created new simple HTML for testing, gave a simple post Messed with index.js to try and limit react to the /api url and was unsuccessful.

--- server/index.js
+++ server/index.js
@@ -6,30 +6,42 @@ const path = require('path');
 const appRand = express();
 const port = 7263; //RAND
 
-
 // Have Node serve the files for our built React app
-appRand.use(express.static(path.resolve(__dirname, '../client/build')));
+// reactAPI.use(express.static(path.resolve(__dirname, '../client/build')));
 
+// appRand.use('/api', reactAPI);
+
+// // Have Node serve the files for our built React app
+// appRand.use(express.static(path.resolve(__dirname, '../client/build')));
 
 appRand.get('/', (request, respond) => {
-    respond.statusCode = 200;
-    respond.json('Hello! This is the front page');
+    // respond.statusCode = 200;
+    // respond.json('Hello! This is the front page');
+    // respond.json('Hello! This is the front page please enter a number');
+    respond.sendFile(path.resolve(__dirname, '../views', 'index.html'));
+
+    console.log(`When exactly does this run and how many times?`);
+
 })
+
+appRand.post('/', (request, respond) => {
+  console.log('There has been a POST request has been made:')
+  console.log(request.query.min);
+  console.log(request.query.max);
+})
+
+
+
 
 // Handle GET requests to /api route
 appRand.get("/api", (request, respond) => {
-  respond.json({ message: "Taylor this is a test message what is happening." });
+  respond.json({ message: "API location this is a test message what is happening." });
 });
 
 // Handle GET requests to /classic route
-appRand.get("/classic", (request, respond) => {
-    respond.sendFile(__dirname + '/index.html');
-  });
-
-// appRand.get('/api', (request, respond) => {
-//     respond.statusCode = 200;
-//     respond.json('Hello from server this is the API page!');
-// })
+// appRand.get("/classic", (request, respond) => {
+//     respond.sendFile(__dirname + '/index.html');
+//   });
 
 appRand.get('/stuff', (request, respond) => {
     respond.statusCode = 404;
@@ -40,7 +52,9 @@ appRand.listen(port, () => {
     console.log(`Success, If you're looking for the app it's on http://localhost:${port}`);
 })
 
-
+// appRand.get('*', (request, respond) => {
+//   respond.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 /*
 
 I dont want this because I was a website with a react side and just an express side, because this is for learning. 
@@ -55,24 +69,24 @@ app.get('*', (request, respond) => {
 });
 */ 
 
-appRand.get('*', (request, respond) => {
-    respond.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
 
-
-
-
-  function checkInput(input) {
+function checkInput(input) {
     var message = "";
-    if(input < 0){
-      message = "No negatives...  User Error! Please Try Again!";
+    if(input.min < 0 | input.max < 0){
+      message = "No negative numbers";
+    }
+    else if(input.min == input.max){
+      message = "Minimum can not be equal to Maximum";
+    }
+    else if(input.min > input.max){
+      message = "Minimum can't be larger than Maximum";
     }
     else {
-      message = "Hi i would love to give you a random number.  " + input;
+      message = `Generating random number for:  [${input.min} : ${input.max}]`;
     }
     return message;
   }
 
-  function getRandomInt(max) {
-    return rand(max+1);
-  };
+function getRandomInt(min, max) {
+  return rand.inBetween(min, max);
+};
